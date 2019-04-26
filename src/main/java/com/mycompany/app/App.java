620758d22ed9b004296a448e9a9bf581364433fb
cursud-devops-ed <48986904+cursud-devops-ed@users.noreply.github.com>
2019-04-26 13:51:07 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -77,7 +77,7 @@ static Connection conn = null;
 	public static void main(String[] argv) {
  
 		//try {
-			log("-------- Connexion au serveur de données MYSQL  Walid ------------");
+			log("-------- Connexion au serveur de données MYSQL  Walid ddd------------");
 			//makeJDBCConnection();
 			log("-------- Afficher toutes les sessions de formations ------------");
 			//getDataFromDB();
